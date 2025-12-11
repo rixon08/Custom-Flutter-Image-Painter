0514f322f786a2fd8d4b3b03d260742228890e98
Skyshi Digital Indonesia <skyshidigitalindonesia@Skyshis-MacBook-Pro.local>
2025-12-11 17:29:53 +0700

    Update padding and color when selected

--- lib/src/_paint_over_image.dart
+++ lib/src/_paint_over_image.dart
@@ -863,9 +863,9 @@ class ImagePainterState extends State<ImagePainter> {
               return Container(
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
-                  color: _controller.mode == PaintMode.none ? Colors.grey[400] : Colors.transparent,
+                  color: _controller.mode == PaintMode.none ? Colors.grey[340] : Colors.transparent,
                 ),
-                padding: const EdgeInsets.all(1),
+                padding: const EdgeInsets.all(13),
                 child: InkWell(
                   onTap: () {
                     if (widget.onPaintModeChanged != null) {
@@ -887,7 +887,7 @@ class ImagePainterState extends State<ImagePainter> {
               return Container(
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
-                  color: _controller.mode != PaintMode.none ? Colors.grey[400] : Colors.transparent,
+                  color: _controller.mode != PaintMode.none ? Colors.grey[340] : Colors.transparent,
                 ),
                 padding: const EdgeInsets.all(5),
                 child: PopupMenuButton(
